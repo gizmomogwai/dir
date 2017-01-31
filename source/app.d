0899de0f85637fbcf937860314f6193723514ef6
Christian Köstlin <christian.koestlin@esrlabs.com>
2017-01-31 22:19:25 +0100

    git columns

--- source/app.d
+++ source/app.d
@@ -2,7 +2,10 @@ import consoled;
 import core.sys.posix.sys.stat;
 import dlib.filesystem.filesystem;
 import dlib.filesystem.local;
+import git.repository;
+import git.status;
 import std.algorithm.sorting;
+import std.algorithm;
 import std.array;
 import std.datetime;
 import std.file : exists, isFile;
@@ -10,6 +13,9 @@ import std.getopt;
 import std.path;
 import std.stdio;
 import std.string;
+import std.traits;
+
+static string DEFAULT_COLUMNS = "GdDo_h_n";
 
 enum SortOrder { byName, dirsFirst }
 
@@ -33,20 +39,25 @@ bool sortByNameDirsFirst(DirEntry a, DirEntry b) {
   }
 }
 
-interface Column {
-  void write(DirEntry entry, stat_t* fileStats);
+class Column {
+  void write(DirEntry entry, stat_t* fileStats) {}
 }
 class NameColumn : Column {
   override void write(DirEntry entry, stat_t* fileStat) {
     writec(entry.name);
+    writec(entry.isDirectory ? "/" : "");
   }
 }
 class DirColorColumn : Column {
   override void write(DirEntry entry, stat_t* fileStat) {
-    writec(entry.isDirectory ? FontStyle.bold : FontStyle.none, entry.isDirectory ? "d" : ".");
+    writec(entry.isDirectory ? FontStyle.underline : FontStyle.none);
   }
 }
-
+class DirMarkerColumn : Column {
+  override void write(DirEntry entry, stat_t* fileStat) {
+    writec(entry.isDirectory ? "d" : ".");
+  }
+}
 class ByteSizeColumn : Column {
   override void write(DirEntry entry, stat_t* fileStat) {
     auto size = fileStat.st_size;
@@ -78,16 +89,14 @@ class HumanReadableSizeColumn : Column {
 
     s = s / 1024.0;
     res = format("%.1fg", s);
-    writeln(res);
+    writecln(res);
     if (res.length <= 4) {
       writec(res);
       return;
     }
-
   }
 }
-
-class RWXColumn : Column {
+class RwxColumn : Column {
   override void write(DirEntry entry, stat_t* fileStat) {
     int mode = fileStat.st_mode;
     auto s = "" ~
@@ -103,103 +112,214 @@ class RWXColumn : Column {
     writec(s);
   }
 }
-
 class OctalColumn : Column {
   override void write(DirEntry entry, stat_t* fileStat) {
     int mode = fileStat.st_mode;
-    writec(format("%4o", mode & 0b111_111_111));
+    writec(format("%3o", mode & 0b111_111_111));
   }
 }
-
 class ModificationTimeColumn : Column {
   override void write(DirEntry entry, stat_t* fileStat) {
-
     auto dt = SysTime.fromUnixTime(fileStat.st_mtime);
     writec(" ", dt.toISOString(), " ");
   }
 }
-
 class SpaceColumn : Column {
   override void write(DirEntry entry, stat_t* fileStat) {
     writec(" ");
   }
 }
-
 class GitColumn : Column {
-  private bool searched;
+  private bool searched = false;
+  private bool found = false;
+  private GitRepo repo;
   override void write(DirEntry entry, stat_t* fileStat) {
+    if (!searched) {
+      this.searched = true;
+      auto repoPath = discoverRepo(".");
+      if (repoPath) {
+        repo = openRepository(repoPath);
+        found = true;
+      }
+    }
+    if (found) {
+      try {
+        withRepo(repo, entry, fileStat);
+        return;
+      } catch (Exception e) {
+      }
+    }
+    withoutRepo(entry, fileStat);
+  }
+  protected void withRepo(GitRepo repo, DirEntry entry, stat_t* fileStat) {
+  }
+  protected void withoutRepo(DirEntry entry, stat_t* fileStat) {
   }
 }
-struct Formatter {
-  Column[] columns;
-  public this(Column[] columns) {
+class GitColorColumn : GitColumn {
+  import deimos.git2.status;
+  override protected void withRepo(GitRepo repo, DirEntry entry, stat_t* fileStat) {
+    auto status = repo.status(entry.name).status;
+    if (status & GIT_STATUS_IGNORED) {
+      writec(Fg.gray);
+    } else {
+      writec(Fg.initial);
+    }
+    if ((status & GIT_STATUS_INDEX_NEW) ||
+        (status & GIT_STATUS_INDEX_MODIFIED) ||
+        (status & GIT_STATUS_INDEX_DELETED) ||
+        (status & GIT_STATUS_INDEX_RENAMED) ||
+        (status & GIT_STATUS_INDEX_TYPECHANGE)) {
+      writec(Bg.lightGreen, Fg.black);
+    } else {
+      if ((status & GIT_STATUS_WT_NEW) ||
+          (status & GIT_STATUS_WT_MODIFIED) ||
+          (status & GIT_STATUS_WT_DELETED) ||
+          (status & GIT_STATUS_WT_TYPECHANGE) ||
+          (status & GIT_STATUS_WT_RENAMED)) {
+        writec(Bg.lightRed, Fg.black);
+      } else {
+        writec(Bg.initial);
+      }
+    }
+  }
+  override protected void withoutRepo(DirEntry entry, stat_t* fileStat) {
+    writec(Fg.initial, Bg.initial);
+  }
+}
+class GitStatusColumn : GitColumn {
+  import deimos.git2.status;
+  override protected void withRepo(GitRepo repo, DirEntry entry, stat_t* fileStat) {
+    writec(toString(repo.status(entry.name).status));
+  }
+  override protected void withoutRepo(DirEntry entry, stat_t* fileStat) {
+    writec("  ");
+  }
+  private string toString(git_status_t status) {
+    dchar index = '-';
+    dchar workTree = '-';
+    if (status & GIT_STATUS_CURRENT) {
+    }
+
+    if (status & GIT_STATUS_INDEX_NEW) {
+      index = 'N';
+    }
+    if (status & GIT_STATUS_INDEX_MODIFIED) {
+      index = 'M';
+    }
+    if (status & GIT_STATUS_INDEX_DELETED) {
+      index = 'D';
+    }
+    if (status & GIT_STATUS_INDEX_RENAMED) {
+      index = 'R';
+    }
+    if (status & GIT_STATUS_INDEX_TYPECHANGE) {
+      index = 'T';
+    }
+
+    if (status & GIT_STATUS_WT_NEW) {
+      workTree = 'N';
+    }
+    if (status & GIT_STATUS_WT_MODIFIED) {
+      workTree = 'M';
+    }
+    if (status & GIT_STATUS_WT_DELETED) {
+      workTree = 'D';
+    }
+    if (status & GIT_STATUS_WT_TYPECHANGE) {
+      workTree = 'T';
+    }
+    if (status & GIT_STATUS_WT_RENAMED) {
+      workTree = 'R';
+    }
+
+    if (status & GIT_STATUS_IGNORED) {
+      index = '-';
+      workTree = '-';
+    }
+    return format("%s%s", index, workTree);
+  }
+}
+
+struct Formatter(T) {
+  T columns;
+
+  public this(T columns) {
     this.columns = columns;
   }
   public void write(string path, DirEntry entry) {
-    foreach (column; columns) {
+    foreach (Column column; columns) {
       stat_t fileStats;
       auto res = stat((path ~ "/" ~ entry.name).toStringz, &fileStats);
       column.write(entry, &fileStats);
     }
-    writeln();
+    writecln(Fg.initial, Bg.initial);
   }
 }
 
-int main2(string[] args) {
-  Column[] columns;
-  void columnsHandler(string option, string value) {
-    foreach (c; value) {
-      switch (c) {
-      case 'd':
-        columns ~= new DirColorColumn();
-        break;
-      case 'f':
-        columns ~= new RWXColumn();
-        break;
-      case 'o':
-        columns ~= new OctalColumn();
-        break;
-      case 'h':
-        columns ~= new HumanReadableSizeColumn();
-        break;
-      case 'b':
-        columns ~= new ByteSizeColumn();
-        break;
-      case 's':
-        columns ~= new SpaceColumn();
-        break;
-      case 'm':
-        columns ~= new ModificationTimeColumn();
-        break;
-      case 'n':
-        columns ~= new NameColumn();
-        break;
-      case 'g':
-        columns ~= new GitColumn();
-        break;
-      default:
-        throw new Exception("unknown option " ~ c);
-      }
-    }
-  }
+public Formatter!(T) getFormatter(T)(T columns) {
+  return Formatter!(T)(columns);
+}
 
-  SortOrder sort;
-  auto helpInformation = getopt(args,
-                                "columns|c", "Specify columns", &columnsHandler,
-                                "sort|s", "Sort mode", &sort,
-  );
+class Columns {
+  Column[dchar] columns;
+  this() {
+    columns = [
+      'D': new DirMarkerColumn(),
+      'G': new GitColorColumn(),
+      '_': new SpaceColumn(),
+      'b': new ByteSizeColumn(),
+      'd': new DirColorColumn(),
+      'f': new RwxColumn(),
+      'h': new HumanReadableSizeColumn(),
+      'm': new ModificationTimeColumn(),
+      'n': new NameColumn(),
+      'o': new OctalColumn(),
+      'g': new GitStatusColumn()
+    ];
+  }
+  Column by(dchar c) {
+    if (c in columns) {
+      return columns[c];
+    }
+    throw new Exception(format("unknown option %c", c));
+  }
+  override string toString() {
+    return columns.
+      byKeyValue().
+      map!("format(\"  %s - %s\", a.key, a.value.classinfo)").
+      join("\n");
+  }
+}
+
+auto sortModeDescription() {
+  return [ EnumMembers!SortOrder ].map!("format(\"  %s\", a)").array.join("\n");
+}
+
+int main(string[] args) {
+  Columns availableColumns = new Columns();
+  SortOrder sort = SortOrder.dirsFirst;
+  string columnsString = DEFAULT_COLUMNS;
+
+  auto helpInformation =
+    getopt(args,
+           "columns|c", format("Columns: (default: '%s')\n%s", DEFAULT_COLUMNS, availableColumns.toString()), &columnsString,
+           "sort|s", "SortOrder: (default: 'dirsFirst')\n" ~ sortModeDescription(), &sort,
+    );
+
   if (helpInformation.helpWanted) {
     defaultGetoptPrinter("listing files flexible.",
                          helpInformation.options);
     return 0;
   }
 
-  string path = ".";
+  auto path = ".";
   if (args.length == 2) {
     path = args[1];
   }
 
-  Formatter formatter = Formatter(columns);
+  auto columns = columnsString.map!((id) {return availableColumns.by(id);}).array;
+  auto formatter = getFormatter(columns);
 
   path = absolutePath(path);
   path = asNormalizedPath(path).array;
@@ -222,55 +342,6 @@ int main2(string[] args) {
   foreach (file; contents) {
     formatter.write(path, file);
   }
+
   return 0;
-
-}
-
-
-import std.stdio : writeln;
-import std.string : toStringz;
-
-import core.memory : GC;
-
-import deimos.git2.buffer;
-import deimos.git2.types;
-import deimos.git2.repository;
-import git.repository;
-import git.oid;
-import git.commit;
-import git.status;
-
-void main() {
-  /*    git_repository *repo;
-
-    git_buf buffer;
-    writeln(git_repository_discover(&buffer, ".".toStringz, cast(bool)true, null));
-    auto repoPath = fromStringz(buffer.ptr);
-    git_buf_free(&buffer);
-    writeln("lowlevel: ", repoPath);
-  */
-    auto repoPath = discoverRepo(".");
-    auto repo = openRepository(repoPath);
-    writeln(repo.head());
-    writeln(repo.state());
-
-
-    auto oid = GitOid("4ee7f63c84bda260b6c61780ab11a229a9721d19");
-    writeln(oid);
-    auto commit = repo.lookupCommit(oid);
-    writeln(commit);
-    writeln(commit.message);
-    writeln(repo.status("source/app.d"));
-
-    foreach (refName, remoteURL, oid, isMerge; repo.walkFetchHead) {
-      writeln(refName);
-      writeln(remoteURL);
-    }
-
-    //writeln("lowlevel: ", git_repository_open(&repo, repoPath.toStringz()));
-    //writeln(buffer);
-    //git_repository_free(repo);
-
-    //    writeln(discoverRepo("."));
-    writeln("END");
 }
